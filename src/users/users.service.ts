8d45038bd47114786ffa1d177218c44791e930dd
Anthony Gibson <anthonygibson24@gmail.com>
2023-01-05 16:29:26 -0600

    all crud operation services written

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { v4 as uuidv4 } from 'uuid';
+import { GetUsersArgs } from './dto/args/get-users.args';
+import { GetUserArgs } from './dto/args/get-user.args';
 import { CreateUserInput } from './dto/input/create-user.input';
 import { UpdateUserInput } from './dto/input/update-user.input';
 import { User } from './models/user';
+import { DeleteUserInput } from './dto/input/delete-user.input';
 
 @Injectable()
 export class UsersService {
@@ -24,17 +27,28 @@ export class UsersService {
         Object.assign(user, updateUserData);
 
         return user;
-
-    public getUser(): User {
-
     }
 
-    public getUsers(): User {
-
+    public getUser(getSingleUser: GetUserArgs): User {
+        return this.users.find((user) => user.userId === getSingleUser.userId);
     }
 
-    public deleteUser(): User {
+    public getUsers(getMultipleUsers: GetUsersArgs): User {
+        // basically what's happening here ^^ is we have a variable 'getMulti..' with a type/class of GetUserArgs. 
+        // by specifying this class, we can access the fields within that class
+        // in this case we want to return the userIds denoted in the GetUserArgs class.
 
+        return getMultipleUsers.userIds.map(userId => this.getUser({ userId }));
+    }
+
+    public deleteUser(deleteUserData: DeleteUserInput): User {
+        const userIndex = this.users.findIndex(user => user.userId === deleteUserData.userId);
+
+        const userToBeDeleted = users[userIndex];
+
+        this.users.splice(userIndex);
+
+        return userToBeDeleted;
     }
 
 }
