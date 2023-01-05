90292e204d1ef652f8d3e2ebdd13efd13ed6528d
Anthony Gibson <anthonygibson24@gmail.com>
2023-01-05 11:05:07 -0600

    added create user and update user services

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as uuidv4 } from 'uuid';
 import { CreateUserInput } from './dto/input/create-user.input';
+import { UpdateUserInput } from './dto/input/update-user.input';
 import { User } from './models/user';
 
 @Injectable()
@@ -8,13 +10,20 @@ export class UsersService {
 
     public createUser(createUserData: CreateUserInput): User {
         const user: User = {
-            userId: 
+            userId: uuidv4(),
+            ...createUserData //spreading create user data so if any additional data is added, it adjusts.
         }
+        this.users.push(user);
+
+        return user;
     }
     
-    public updateUser(): User {
+    public updateUser(updateUserData: UpdateUserInput): User {
+        const user = this.users.find(user => user.userId === updateUserData.userId)
 
-    }
+        Object.assign(user, updateUserData);
+
+        return user;
 
     public getUser(): User {
 
